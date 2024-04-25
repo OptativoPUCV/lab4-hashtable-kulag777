e027a6ffb33b6d702cb320dbbaa966fe03411c2a
kulag777 <163082316+kulag777@users.noreply.github.com>
2024-04-25 23:53:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -75,7 +75,21 @@ HashMap *createMap(long capacity) {
 }
 void eraseMap(HashMap *map, char *key) {}
 
-Pair *searchMap(HashMap *map, char *key) { return NULL; }
+Pair *searchMap(HashMap *map, char *key) {
+  if (map == NULL || key == NULL) {
+    return NULL;
+  }
+  long posicion = hash(key, map->capacity);
+  while (map->buckets[posicion] != NULL &&
+         map->buckets[posicion]->key != NULL) {
+    if (is_equal(map->buckets[posicion]->key, key)) {
+      map->current = posicion;
+      return map->buckets[posicion];
+    }
+    posicion = (posicion + 1) % map->capacity;
+  }
+  return NULL;
+}
 
 Pair *firstMap(HashMap *map) { return NULL; }
 
